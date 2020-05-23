fde8b0d560db207e735815c5c5aeb4b29d6b9c94
Ruvidu <mruv98@gmail.com>
2020-05-23 18:40:04 +0530

    Scala assignment1

---
+++ scalafunctions.scala
@@ -0,0 +1,8 @@
+def convertf(x:Double): Double={x*1.8 +32}
+convertf(35)
+
+def findvolume(r:Double): Double={4.0/3*Math.PI*r*r*r}
+findvolume(5)
+
+def cost(y:Int): Double = {y*24.95*60/100 + 3*50 + (y-50)*0.75}
+cost(60)
