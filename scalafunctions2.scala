4d8433b46b3a6eefb7319214565ad54d4f530fd6
Ruvidu <mruv98@gmail.com>
2020-05-28 19:39:12 +0530

    Scala assignment2

---
+++ scalafunctions2.scala
@@ -0,0 +1,19 @@
+//1
+def normalpay(hours:Int):Int=hours*150
+
+def otpay(hours:Int):Int=hours*75
+
+def fullpay(h1:Int,h2:Int):Int=normalpay(h1)+otpay(h2)
+
+def tax(fullpay:Int):Double=fullpay*0.1
+
+def takehome(h1:Int,h2:Int):Double=fullpay(h1,h2)-tax(fullpay(h1,h2))
+
+//2
+def attendees(price:Int):Int=120+(15-price)/5*20
+
+def revenue(price:Int):Int=attendees(price)*price
+
+def cost(price:Int):Int=attendees(price)*3+500
+
+def profit(price:Int):Int=revenue(price)-cost(price)
